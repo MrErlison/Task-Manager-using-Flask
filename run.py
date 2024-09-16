4eff1dd1bd4c38d8504024bba32b3015483385e0
Mr. Erlison <sisal.cordame_0a@icloud.com>
2024-09-15 21:34:33 -0300

    fix Anti-clickjacking and Header Not Set

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 from todo_project import app
 
+# Adicionar os cabeçalhos de segurança
+@app.after_request
+def add_security_headers(response):
+    response.headers['X-Frame-Options'] = 'DENY'  # Para evitar que a página seja carregada em iframes
+    response.headers['Content-Security-Policy'] = (
+        "frame-ancestors 'none';"  # Usando CSP para evitar o embutimento
+        "default-src 'self'; "  # Permitir recursos apenas do próprio domínio
+        "script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net; "  # JavaScript do próprio site e de fontes confiáveis
+        "style-src 'self' https://cdn.jsdelivr.net; "  # CSS apenas do próprio domínio e de fontes confiáveis
+        "img-src 'self' data:; "  # Permitir imagens do próprio domínio e inline (base64)
+        "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; "  # Permitir fontes do Google Fonts
+    )
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
